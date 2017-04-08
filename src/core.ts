c402e7538e309d11b3be9b02c5db6bf3b2ddc18d
Alexei Volkov <lifecoder.ua@gmail.com>
2017-04-08 12:34:39 +0300

    basic output

--- src/core.ts
+++ src/core.ts
@@ -3,7 +3,9 @@ import { Render } from './render'
 import { Map } from './map'
 
 function run() {
-  var map = new Map()
+  let map = new Map()
+  let render = new Render()
+  render.render(map)
   console.log(`map: ${map}`)
   console.log((new Extra()).cool())
 }
--- src/render.ts
+++ src/render.ts
@@ -1,8 +1,10 @@
 export class Render {
   // map = []
+  target = document.getElementById('render-target')
 
+  render(map) {
+    this.target.innerHTML = ''
 
-  render() {
-
+    map.map.map( (row) => this.target.innerHTML += `${row.join('')}<br>` )
   }
 }
